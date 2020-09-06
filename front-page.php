17828b847590947bbbff0684de3ec66bed9ef8ca
Coman George <coman.george1998@gmail.com>
2020-09-06 14:01:54 +0300

    First Commit

---
+++ front-page.php
@@ -0,0 +1,138 @@
+<?php get_header(); ?>
+
+  <div class="page-banner">
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>);"></div>
+    <div class="page-banner__content container t-center c-white">
+      <h1 class="headline headline--large">Hello!</h1>
+      <h2 class="headline headline--medium">Be part to one of the best universities in the world</h2>
+      <h3 class="headline headline--small">Take a look to our <strong>programs</strong></h3>
+      <a href="<?php echo get_post_type_archive_link('program'); ?>" id="buttonAnimationOne"class="btn btn--large btn--blue">Programs</a>
+    </div>
+  </div>
+
+  <div class="full-width-split group">
+    <div class="full-width-split__one">
+      <div class="full-width-split__inner">
+        <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
+
+        <?php
+          $today = date('Ymd');
+          $homepageEvents = new WP_Query(array(
+            'posts_per_page' => 2,
+            'post_type' => 'event',
+            'meta_key' => 'event_date',
+            'orderby' => 'meta_value_num',
+            'order' => 'ASC',
+            'meta_query' => array(
+              array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
+              )
+            )
+          ));
+
+          while($homepageEvents->have_posts()) {
+            $homepageEvents->the_post();
+            get_template_part('template-parts/content', 'event');
+          }
+        ?>
+
+        <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event') ?>" id="gradientButton" class="btn btn--blue">View All Events</a></p>
+
+      </div>
+    </div>
+    <div class="full-width-split__two">
+      <div class="full-width-split__inner">
+        <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
+        <?php
+          $homepagePosts = new WP_Query(array(
+            'posts_per_page' => 1
+          ));
+
+          while ($homepagePosts->have_posts()) {
+            $homepagePosts->the_post(); ?>
+            <div class="event-summary">
+              <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
+                <span class="event-summary__month"><?php the_time('M'); ?></span>
+                <span class="event-summary__day"><?php the_time('d'); ?></span>
+              </a>
+              <div class="event-summary__content">
+                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                <p><?php if (has_excerpt()) {
+                    echo get_the_excerpt();
+                  } else {
+                    echo wp_trim_words(get_the_content(), 18);
+                    } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+              </div>
+            </div>
+          <?php } wp_reset_postdata();
+        ?>
+
+
+
+
+        <p class="t-center no-margin"><a href="<?php echo site_url('/blog'); ?>" class="btn btn--yellow">View All Blog Posts</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="hero-slider">
+    <?php
+      $homePageSlides = new WP_Query(array(
+        'post_type' => 'slide'
+      ));
+      if ($homePageSlides->have_posts()){
+      while ($homePageSlides->have_posts()){
+        $homePageSlides->the_post()
+        ?>
+    <div class="hero-slider__slide" style="background-image: url(<?php $backgroundImg = get_field('slide_background'); echo $backgroundImg['url']; ?>);">
+      <div class="hero-slider__interior container">
+        <div class="hero-slider__overlay">
+          <h2 class="headline headline--medium t-center"><?php the_title(); ?></h2>
+          <p class="t-center"><?php the_field('slide_subtitle');  ?></p>
+          <p class="t-center no-margin"><a href="#" class="btn btn--blue"><?php the_field('slide_button_text') ?></a></p>
+        </div>
+      </div>
+    </div>
+    <?php
+      }
+    } else { ?>
+      <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bus.jpg') ?>);">
+            <div class="hero-slider__interior container">
+              <div class="hero-slider__overlay">
+                <h2 class="headline headline--medium t-center">Free Transportation</h2>
+                <p class="t-center">All students have free unlimited bus fare.</p>
+                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/apples.jpg') ?>);">
+            <div class="hero-slider__interior container">
+              <div class="hero-slider__overlay">
+                <h2 class="headline headline--medium t-center">An Apple a Day</h2>
+                <p class="t-center">Our dentistry program recommends eating apples.</p>
+                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bread.jpg') ?>);">
+            <div class="hero-slider__interior container">
+              <div class="hero-slider__overlay">
+                <h2 class="headline headline--medium t-center">Free Food</h2>
+                <p class="t-center">Fictional University offers lunch plans for those in need.</p>
+                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+     <?php } ?>
+
+
+  </div>
+
+</div>
+
+  <?php get_footer();
+
+?>
